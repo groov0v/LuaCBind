ed438b1eade6f02a9046a40b368d2109eff8190a
Heath Luo <Heath@Heaths-MacBook-Pro.local>
2013-01-08 21:49:39 +0800

    Initial Commit

---
+++ LuaCBind/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  LuaCBind
+//
+//  Created by Heath Luo on 8/1/13.
+//  Copyright (c) 2013 Heath Luo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
+
